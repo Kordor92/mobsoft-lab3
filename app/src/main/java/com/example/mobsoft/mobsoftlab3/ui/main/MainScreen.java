7a2c6efb0437479e8a82c46ca6aa25212fee88d1
Kordor92 <jozsefszegletes@gmail.com>
2017-03-24 09:50:40 +0100

    travis first

---
+++ app/src/main/java/com/example/mobsoft/mobsoftlab3/ui/main/MainScreen.java
@@ -0,0 +1,14 @@
+package com.example.mobsoft.mobsoftlab3.ui.main;
+
+/**
+ * Created by mobsoft on 2017. 03. 24..
+ */
+
+
+/**
+ * Created by Patrik on 2017. 03. 19..
+ */
+
+public interface MainScreen {
+    void showMessage(String text);
+}
